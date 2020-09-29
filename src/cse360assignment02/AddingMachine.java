9455c4b098f8867f001966d8af05b52e11ac6f9b
Jake <jake.cohen42@gmail.com>
2020-09-29 10:50:11 -0700

    Version 1 - the initial version of this code does not do anything, but instead defines the method.

---
+++ src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,32 @@
+package cse360assignment02;
+
+public class AddingMachine {
+    private int total;
+
+    //Class constructor
+    public AddingMachine () {
+        total = 0;  // not needed - included for clarity
+    }
+
+    //Returns the variable total
+    public int getTotal () {
+        return 0;
+    }
+
+    //Adds the parameter 'value' to the total
+    public void add (int value) {
+    }
+
+    //Subtracts the parameter 'value' to the total
+    public void subtract (int value) {
+    }
+
+    //Returns the total as a string
+    public String toString () {
+        return "";
+    }
+
+    //Resets 'total' to 0
+    public void clear() {
+    }
+}
